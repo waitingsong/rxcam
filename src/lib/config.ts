cb43fcc9b0c9c4e25ed9ff1092de6bfec54c4c8e
waiting <1661926154@qq.com>
2018-04-26 14:42:29 +0800

    chore: format

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,15 +3,12 @@ import {
   VideoIdx,
 } from './model'
 
-export let mediaDevices: MediaDevices
 
 // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  mediaDevices = navigator.mediaDevices
-}
-else {
+if (! navigator.mediaDevices || ! navigator.mediaDevices.getUserMedia) {
   throw new Error('mediaDevices.getUserMedia not support')
 }
 
+export const mediaDevices: MediaDevices = navigator.mediaDevices
 export const deviceMap = new Map<DeviceId, MediaDeviceInfo>()
 export const videoIdxMap = new Map<VideoIdx, DeviceId>()
