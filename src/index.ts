28cb4c7d40bd04be619548707c6bd0b3a4199a1c
waiting <1661926154@qq.com>
2018-05-22 14:49:58 +0800

    chore: format

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-import { resetDeviceInfo } from './lib/device'
-import { init, RxCam } from './lib/index'
-import { calcImgThumbResolution, exportFromCanvas, genCanvas, takeThumbnail } from './lib/media'
-
-
+export { resetDeviceInfo } from './lib/device'
+export { init, RxCam } from './lib/index'
+export { calcImgThumbResolution, exportFromCanvas, genCanvas, takeThumbnail } from './lib/media'
 export * from './lib/model'
-export {
-  RxCam,
-  init,
-
-  calcImgThumbResolution,
-  exportFromCanvas,
-  genCanvas,
-  resetDeviceInfo,
-  takeThumbnail,
-}
