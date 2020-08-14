29d2c0690b56362c18d0a7e08b60d1c3d2020f99
waiting <1661926154@qq.com>
2020-08-14 23:07:45 +0800

    chore: fix for UOS

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -12,6 +12,34 @@ import {
 } from './model'
 
 
+// fix for UOS
+if (typeof navigator.mediaDevices === 'undefined') {
+  // @ts-ignore
+  navigator.mediaDevices = {}
+}
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+if (typeof navigator.mediaDevices.getUserMedia === 'undefined') {
+  navigator.mediaDevices.getUserMedia = constraints => {
+    // First get ahold of the legacy getUserMedia, if present
+    // @ts-ignore
+    const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia
+
+    // Some browsers just don't implement it - return a rejected promise with an error
+    // to keep a consistent interface
+    if (!getUserMedia) {
+      return Promise.reject(new Error('getUserMedia is not implemented in this browser'))
+    }
+
+    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+    return new Promise((resolve, reject) => {
+      // @ts-ignore
+      getUserMedia.call(navigator, constraints, resolve, reject)
+    })
+  }
+}
+
 if (location && location.protocol && location.protocol.toLowerCase().indexOf('https') === 0) {
   // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
   if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
